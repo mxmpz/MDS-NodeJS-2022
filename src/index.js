51409a44ad0b7b451f865a06e876e863394302e3
Maxime Prouzat <maximeprouzat6@gmail.com>
2022-10-18 16:22:48 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -17,14 +17,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const connect = require('./data/helpers/db')
+const User = require('./data/models/User')
+
 //On importe le logger
 const logger = require('./middlewares/logger')
 //On dit à Express d'utiliser le logger en tant que middleware
 app.use(logger)
 
+connect()
 
 app.get('/', (req, res) => {
     res.send('Hello world TOTO !')
+})
+
+app.post('/user', (req, res) => {
+    const user = new User({
+        firstName: 'Michel',
+        lastName: 'Toto',
+        phone: '010203040506',
+        email: 'michel.toto@mail.com',
+        password: 'SuperPassword44'
+    })
+    user.save()
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error))
+
+    res.send()
 })
 
 app.listen(port, () => {
