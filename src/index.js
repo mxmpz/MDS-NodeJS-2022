8552b024addb188ebd510f9afbfed60adbd08f26
Maxime Prouzat <maximeprouzat6@gmail.com>
2022-10-18 12:39:47 +0200

    add logger middleware

--- src/index.js
+++ src/index.js
@@ -15,8 +15,13 @@
 
 const express = require('express')
 const app = express()
-
 const port = 3000
+
+//On importe le logger
+const logger = require('./middlewares/logger')
+//On dit à Express d'utiliser le logger en tant que middleware
+app.use(logger)
+
 
 app.get('/', (req, res) => {
     res.send('Hello world TOTO !')
---
+++ src/middlewares/logger.js
@@ -0,0 +1,18 @@
+/**
+ * Middleware to log all request
+ * @param {*} req HTTP Request
+ * @param {*} res HTTP Response
+ * @param {*} next next() middleware function
+ */
+
+const loggerMiddleware = (req, res, next) => {
+    if(req) {
+        //Ancienne méthode
+        //console.info('[' + new Date().toLocaleString() + '] Request' + req.method + 'from' + req.ip + ' to ' + req.url)
+        //Nouvelle méthode
+        console.info(`[${new Date().toLocaleString()}] Requet ${req.method} from ${req.ip} to ${req.url}`)
+    }
+    next()
+}
+
+module.exports = loggerMiddleware
