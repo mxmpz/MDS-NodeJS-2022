8a40ffae930792a8ec9efdee14c3d80ec3a59702
Maxime Prouzat <maximeprouzat6@gmail.com>
2022-10-18 12:15:46 +0200

    commit 2

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const http = require('http')
+//Version a l'ancienne
 
-//Ancienne méthode
+// const http = require('http')
+
+ //Ancienne méthode
+ // const server = http.createServer((req,res) => {
+ // })
+
+ //Nouvelle méthode avec fonction fléchée
 // const server = http.createServer((req,res) => {
+//     res.end('Ceci est la réponse de mon serveur')
 // })
 
-//Nouvelle méthode avec fonction fléchée
-const server = http.createServer((req,res) => {
-    res.end('Ceci est la réponse de mon serveur')
+// server.listen(3000)
+
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello world TOTO !')
 })
 
-server.listen(3000)
+app.listen(port, () => {
+    console.log('Server is running on port' + port)
+})
