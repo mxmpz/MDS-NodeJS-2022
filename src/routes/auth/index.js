35e01801fe274973022981faf4dcd4fbe7ba583f
Maxime Prouzat <maximeprouzat6@gmail.com>
2022-11-02 15:03:41 +0100

    credentials modif

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -7,16 +7,28 @@ router.route('/login')
     const credentials = req.body
 
     if (!credentials.email || !credentials.password) {
-      return res.status(403).send('Invalid crednetials')
+      return res.status(403).send('Invalid credentials')
     }
 
     try {
       const user = await User.findOne({ email: credentials.email })
 
       if (!user) {
-        return res.status(403).send('Invalid crednetials')
+        return res.status(403).send('Invalid credentials')
       }
       // if (!user) return res.status(403).send('Invalid crednetials')
+
+      user.comparePassword(credentials.password, (error, isMatch) => {
+        if (error) {
+          return res.status(403).send('Invalid credentials')
+        }
+
+        if (isMatch) {
+          console.log('OK')
+        } else {
+          console.log('KO')
+        }
+      })
     } catch (error) {
       console.error(error)
       res.status(500).send('Invalid crednetials')
