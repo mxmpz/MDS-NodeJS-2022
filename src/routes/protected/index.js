63d7ec5f958f40c2600dcf1ee524054ea2b1f401
Maxime Prouzat <maximeprouzat6@gmail.com>
2022-11-02 16:33:18 +0100

    protected decode

--- src/routes/protected/index.js
+++ src/routes/protected/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
 
 router.route('/')
   .get((req, res) => {
     const headers = req.headers
     console.log(headers)
+    const decoded = jwt.verify(req.headers.authorization.split(' ')[1], 'MON SUPER MOT DE PASSE SECRET')
+    console.log(decoded)
     return res.send('COUCOU')
   })
 
