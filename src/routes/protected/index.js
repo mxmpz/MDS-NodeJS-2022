dce53fe1e03a740434f9d21c55232dfd6c8283b3
Maxime Prouzat <maximeprouzat6@gmail.com>
2022-11-02 16:30:25 +0100

    protected headers

--- src/routes/protected/index.js
+++ src/routes/protected/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 
 router.route('/')
   .get((req, res) => {
+    const headers = req.headers
+    console.log(headers)
     return res.send('COUCOU')
   })
 
