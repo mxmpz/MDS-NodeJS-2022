125f78723e0f69f03131b1954465226a73e6cf6a
Maxime Prouzat <maximeprouzat6@gmail.com>
2022-12-14 09:18:02 +0100

    comment

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -32,10 +32,10 @@ router.route('/login')
             if (error) {
               return res.status(500).send('Invalid credentials')
             }
-
+            // On supprime le mot de passe de l'utilisateur récupéré en base
             const _user = user.toObject()
             delete _user.password
-
+            // On retourne l'utilisateur et le token
             return res.send({
               user,
               token
