6e261838f4e75aa5ccca550b3641a99d207b217a
Maxime Prouzat <maximeprouzat6@gmail.com>
2023-01-05 16:22:56 +0100

    download

--- src/routes/files/download/index.js
+++ src/routes/files/download/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const { dirname } = require('path')
+const withAuth = require('../../../middlewares/auth')
 const appDir = dirname(require.main.filename)
-
+const File = require('../../../data/models/File')
 const filesDir = appDir + '/../files/'
 
 router.route('/')
-  .get((req, res) => {
+  .get(withAuth, (req, res) => {
     const { body } = req
 
     if (!body.fileName) {
@@ -16,6 +17,23 @@ router.route('/')
       console.error(error)
       return res.status(500).send('error')
     })
+  })
+
+router.route('/:id')
+  .get(withAuth, async (req, res) => {
+    const { id } = req.params
+
+    const file = await File.findById(id)
+    if (file) {
+      const fileObject = file.toObject()
+      if (fileObject.user.equals(req.userId)) {
+        return res.send(fileObject)
+      } else {
+        return res.status(402).send('You are not authorized to access this file')
+      }
+    } else {
+      return res.status(404).send('File not found')
+    }
   })
 
 module.exports = router
